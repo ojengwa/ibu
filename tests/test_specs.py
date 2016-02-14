2fb6d7bc3d7052b74dd93bcd3d9f0f9f6c9b5558
Bernard Ojengwa <bernardojengwa@gmail.com>
2016-02-14 21:07:36 +0100

    add more tests

--- tests/test_specs.py
+++ tests/test_specs.py
@@ -1,7 +1,31 @@
+"""Summary."""
 import unittest
+
+from click.testing import CliRunner
 
 
 class TestCommand(unittest.TestCase):
+    """
+    Summary.
+
+    Attributes:
+        runner (TYPE): Description
+    """
+
+    def setUp(self):
+        """
+        Summary.
+
+        Returns:
+            TYPE: Description
+        """
+        self.runner = CliRunner()
 
     def test_is_true(self):
+        """
+        Summary.
+
+        Returns:
+            TYPE: Description
+        """
         self.assertTrue(True)
