d570d5744a1941fe0a0a6fc3b4bc89cd5aca34a1
Bernard Ojengwa <bernardojengwa@gmail.com>
2016-02-20 19:07:31 +0100

    specify more options for command group

--- ibu/cli.py
+++ ibu/cli.py
@@ -1,4 +1,10 @@
 # -*- coding: utf-8 -*-
+"""
+Summary.
+
+Attributes:
+    CONTEXT_SETTINGS (TYPE): Description
+"""
 from __future__ import print_function
 
 import click
@@ -7,12 +13,38 @@ import click
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
 
-@click.group()
-def ibu():
-    """This is still a work in progress."""
-    pass
+@click.group(name='ibu')
+@click.option('-d/-s', '--debug/--silent', default=False)
+@click.option('-v', '--version')
+def cli():
+    """
+    Summary.
+
+    Returns:
+        TYPE: Description
+    """
 
 
-@click.command(context_settings=CONTEXT_SETTINGS)
-def test():
+@cli.command(context_settings=CONTEXT_SETTINGS)
+def fixture():
+    """
+    Summary.
+
+    Returns:
+        TYPE: Description
+    """
     print("hello")
+
+
+@cli.command(context_settings=CONTEXT_SETTINGS)
+def schema():
+    """
+    Summary.
+
+    Returns:
+        TYPE: Description
+    """
+    print("hello")
+
+if __name__ == '__main__':
+    cli()
